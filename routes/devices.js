2524959811aefd4f8da3121962f9a781e9d147d4
Hugo Caille <hugo.caille@orange.fr>
2015-06-28 23:17:43 +0200

    Get device by name

--- routes/devices.js
+++ routes/devices.js
@@ -8,13 +8,25 @@ var Models = require('../models');
 router.use(passport.authenticate('bearer', {session: false}));
 
 router.get('/', function(req, res) {
-  Models.Device.findAll({
-    where: {
-      userId: req.user.id
-    }
-  }).then(function(result) {
-    res.json(result);
-  });
+  if (req.query['name'] === undefined) {
+    Models.Device.findAll({
+      where: {
+        UserId: req.user.id
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  }
+  else {
+    Models.Device.findOne({
+      where: {
+        UserId: req.user.id,
+        name: req.query['name']
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  }
 });
 
 router.post('/', function(req, res) {
@@ -23,6 +35,30 @@ router.post('/', function(req, res) {
     name: req.body['name']
   }).then(function() {
     res.sendStatus(200);
+  });
+});
+
+router.get('/:id', function(req, res) {
+  Models.Device.findOne({
+    where: {
+      id: req.params['id'],
+      UserId: req.user.id
+    }
+  }).then(function(device) {
+    res.json(device);
+  });
+})
+
+router.delete('/:id', function(req, res) {
+  Models.Device.findOne({
+    where: {
+      id: req.params['id'],
+      UserId: req.user.id
+    }
+  }).then(function(device) {
+    device.destroy().then(function() {
+      res.sendStatus(204);
+    });
   });
 });
 
