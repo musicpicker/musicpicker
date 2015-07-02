887e01e37a9df55f9a4db4485bb3a82a3344c0b4
Hugo Caille <hugo.caille@isen-lille.fr>
2015-07-02 20:22:07 +0200

    Setting default view engine

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ var devices = require('./routes/devices');
 var metadata = require('./routes/metadata');
 
 var app = express();
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({limit: '10mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
