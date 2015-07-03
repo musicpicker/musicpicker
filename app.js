708659a21deabe822de71e42138c3a515db2b588
Hugo Caille <hugo.caille@isen-lille.fr>
2015-07-03 04:30:21 +0200

    Fixing express 500 error

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var devices = require('./routes/devices');
 var metadata = require('./routes/metadata');
 
 var app = express();
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
