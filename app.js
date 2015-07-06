55c37eacd3ceab323ab29e19fd520c4c3ee741c7
Hugo Caille <hugo.caille@isen-lille.fr>
2015-07-06 19:35:54 +0200

    Replaced default error views with express-error-handler

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var errorHandler = require('express-error-handler')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -13,8 +14,7 @@ var devices = require('./routes/devices');
 var metadata = require('./routes/metadata');
 
 var app = express();
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(errorHandler());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -30,37 +30,5 @@ app.use('/oauth', auth);
 app.use('/api/account', account);
 app.use('/api/devices', devices);
 app.use('/api', metadata);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
