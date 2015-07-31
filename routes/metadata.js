88fc915e8b843200159eafa954bef283ccc06275
Hugo Caille <hugo.caille@orange.fr>
2015-07-31 10:21:39 +0200

    Device ownership check in metadata routes

--- routes/metadata.js
+++ routes/metadata.js
@@ -6,25 +6,34 @@ var statsd = require('../statsd').middleware;
 
 router.get('/tracks', function(req, res) {
   var deviceId = req.query['device'];
-  var request = knex.select('tracks.*').from('tracks').
+
+  new models.Device({
+    Id: deviceId,
+    OwnerId: req.user.id
+  }).fetch().then(function(device) {
+    var request = knex.select('tracks.*').from('tracks').
       innerJoin('deviceTracks', 'tracks.Id', 'deviceTracks.TrackId').
       where('deviceTracks.DeviceId', req.query['device']).orderBy('tracks.Number');
-  if (req.query['album'] === undefined) {
-    req.statsdKey = 'http.meta-tracks-list.get';
-    request.then(function(result) {
-      res.json(result);
-    }).catch(function(err) {
-      res.sendStatus(500);
-    });
-  }
-  else {
-    req.statsdKey = 'http.meta-tracks-list-filter.get';
-    request.where('tracks.AlbumId', req.query['album']).then(function(result) {
-      res.json(result);
-    }).catch(function(err) {
-      res.sendStatus(500);
-    });
-  }
+
+	  if (req.query['album'] === undefined) {
+	    req.statsdKey = 'http.meta-tracks-list.get';
+	    request.then(function(result) {
+	      res.json(result);
+	    }).catch(function(err) {
+	      res.sendStatus(500);
+	    });
+	  }
+	  else {
+	    req.statsdKey = 'http.meta-tracks-list-filter.get';
+	    request.where('tracks.AlbumId', req.query['album']).then(function(result) {
+	      res.json(result);
+	    }).catch(function(err) {
+	      res.sendStatus(500);
+	    });
+	  }
+  }).catch(function(err) {
+    return res.sendStatus(404);
+  });
 });
 
 router.get('/tracks/:id', statsd('meta-tracks-detail'), function(req, res) {
@@ -37,26 +46,34 @@ router.get('/tracks/:id', statsd('meta-tracks-detail'), function(req, res) {
 
 router.get('/albums', function(req, res) {
   var deviceId = req.query['device'];
-  var request = knex.select('albums.*').from('tracks').innerJoin('deviceTracks', 'tracks.Id', 'deviceTracks.TrackId').
-    where('deviceTracks.DeviceId', req.query['device']).innerJoin('albums', 'tracks.AlbumId', 'albums.Id').
-    distinct('albums.Id').orderBy('albums.Name');
 
-  if (req.query['artist'] === undefined) {
-    req.statsdKey = 'http.meta-albums-list.get';
-    request.then(function(result) {
-      res.json(result);
-    }).catch(function(err) {
-      res.sendStatus(500);
-    });
-  }
-  else {
-    req.statsdKey = 'http.meta-albums-list-filter.get';
-    request.where('albums.ArtistId', req.query['artist']).then(function(result) {
-      res.json(result);
-    }).catch(function(err) {
-      res.sendStatus(500);
-    });
-  }
+  new models.Device({
+    Id: deviceId,
+    OwnerId: req.user.id
+  }).fetch().then(function(device) {
+	  var request = knex.select('albums.*').from('tracks').innerJoin('deviceTracks', 'tracks.Id', 'deviceTracks.TrackId').
+	    where('deviceTracks.DeviceId', req.query['device']).innerJoin('albums', 'tracks.AlbumId', 'albums.Id').
+	    distinct('albums.Id').orderBy('albums.Name');
+
+	  if (req.query['artist'] === undefined) {
+	    req.statsdKey = 'http.meta-albums-list.get';
+	    request.then(function(result) {
+	      res.json(result);
+	    }).catch(function(err) {
+	      res.sendStatus(500);
+	    });
+	  }
+	  else {
+	    req.statsdKey = 'http.meta-albums-list-filter.get';
+	    request.where('albums.ArtistId', req.query['artist']).then(function(result) {
+	      res.json(result);
+	    }).catch(function(err) {
+	      res.sendStatus(500);
+	    });
+	  }
+	}).catch(function() {
+		return res.sendStatus(404);
+	})
 });
 
 router.get('/albums/:id', statsd('meta-albums-detail'), function(req, res) {
@@ -69,15 +86,22 @@ router.get('/albums/:id', statsd('meta-albums-detail'), function(req, res) {
 
 router.get('/artists', statsd('meta-artists-list'), function(req, res) {
   var deviceId = req.query['device'];
-  var request = knex.select('artists.*').from('tracks').innerJoin('deviceTracks', 'tracks.Id', 'deviceTracks.TrackId').
-    where('deviceTracks.DeviceId', req.query['device']).innerJoin('albums', 'tracks.AlbumId', 'albums.Id').
-    innerJoin('artists', 'albums.ArtistId', 'artists.Id').distinct('artists.Id').orderBy('artists.Name');
+  new models.Device({
+    Id: deviceId,
+    OwnerId: req.user.id
+  }).fetch().then(function(device) {
+	  var request = knex.select('artists.*').from('tracks').innerJoin('deviceTracks', 'tracks.Id', 'deviceTracks.TrackId').
+	    where('deviceTracks.DeviceId', req.query['device']).innerJoin('albums', 'tracks.AlbumId', 'albums.Id').
+	    innerJoin('artists', 'albums.ArtistId', 'artists.Id').distinct('artists.Id').orderBy('artists.Name');
 
-    request.then(function(result) {
-      res.json(result);
-    }).catch(function(err) {
-      res.sendStatus(500);
-    });
+	    request.then(function(result) {
+	      res.json(result);
+	    }).catch(function(err) {
+	      res.sendStatus(500);
+	    });
+	  }).catch(function() {
+	  	return res.sendStatus(404);
+	  });
 });
 
 router.get('/artists/:id', statsd('meta-artists-detail'), function(req, res) {
