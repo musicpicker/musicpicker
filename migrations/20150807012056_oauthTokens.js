68a5ecca44f36caaa271124cded1926951af3365
Hugo Caille <hugo.caille@orange.fr>
2015-08-08 00:41:59 +0200

    Fixed PostgreSQL migrations

--- migrations/20150807012056_oauthTokens.js
+++ migrations/20150807012056_oauthTokens.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   var tokens = knex.schema.createTable('oauth_tokens', function(table) {
   	table.string('token').primary();
 		table.integer('user').references('Id').inTable('users');
-		table.integer('client').references('Id').inTable('oauth_apps');
+		table.integer('client').references('id').inTable('oauth_apps');
 		table.timestamps();
   });
 
